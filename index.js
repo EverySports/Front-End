abad7987aa435a3e99a73f1309119339fb35e410
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2020-07-29 11:24:47 +0900

    delete prev src dir

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
-import configureStore from './src_/modules';
+import configureStore from './src/modules';
 
 const store = configureStore;
 
