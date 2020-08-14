a602b0a32a7b97bf4fa6b2ea4f18580283e042d2
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2020-08-14 18:11:45 +0900

    add animated
    add animated

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,7 +1,14 @@
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, Text, Image, Animated} from 'react-native';
+import Login from './Login/Login';
+import EverySportsLogo from '../assets/img/EverySports.png';
 
 const Loading = ({navigation}) => {
+  const [state, setState] = useState({
+    animatedDone: false,
+    animatedValue: new Animated.Value(0),
+  });
+
   useEffect(() => {
     setTimeout(() => {
       // navigation.navigate('login');
@@ -9,10 +16,29 @@ const Loading = ({navigation}) => {
   }, []);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text> Loading </Text>
+    <View style={styles.container}>
+      <View style={styles.animated}>
+        <Image source={EverySportsLogo} style={styles.imgLogo} />
+      </View>
+      {state.animatedDone && <Login />}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  animated: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    transform: [{translateY: -200}],
+  },
+  imgLogo: {
+    width: 300,
+    height: 170,
+  },
+});
 
 export default Loading;
