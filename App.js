4ec5c58e8054971ee7a81fe76e66403568064e16
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2020-08-27 20:51:18 +0900

    -

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import Loading from './src/screens/Loading';
 import Login from './src/screens/Login/Login';
 import BottomTabNavigator from './src/screens/Main/BottomTabNavigator';
-
+// Set WebRTC .... another pj....
 const AppSwitchNavigator = createSwitchNavigator(
   {
     loading: Loading,
