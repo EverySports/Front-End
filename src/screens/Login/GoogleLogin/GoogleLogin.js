7db658da9546e3a6ed5dc0b1297baffcb5dd973a
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2020-09-09 20:28:17 +0900

    Update GoogleLogin.js

--- src/screens/Login/GoogleLogin/GoogleLogin.js
+++ src/screens/Login/GoogleLogin/GoogleLogin.js
@@ -26,27 +26,6 @@ import GoogleSignup from './GoogleSignup';
 // 5. 백 : 로그인 사용자 정보를 프론트로 전송, 프론트 : global state에 로그인 유저 정보 저장
 // 6. navigate main screens
 
-const _userInfo = {
-  idToken:
-    'eyJhbGciOiJSUzI1NiIsImtpZCI6IjNmMzMyYjNlOWI5MjhiZmU1MWJjZjRmOGRhNTQzY2M0YmQ5ZDQ3MjQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzOTM3NjM3NDE3NzYtbmM1ODg5MGp2NDE2NmgwMnNlZHEzamYwZTFnMXE0aHYuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIzOTM3NjM3NDE3NzYtb2x0YXJxaGU3MGFvZGlmaW1qcGxyMjdlYmdnNG82YTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDMxMDk1MzMxMjUxMTI3NzMyODgiLCJlbWFpbCI6Imp3eTI3NzcyNDE1QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiM3hiWFkzQUtJdGdxTTIyUlc4SUhKUSIsIm5vbmNlIjoibTM1TmNjd3dfY1RWczZaZjh5OWhZa1hIc1c1bHh5Y0dnaUdmWHNYcTA1dyIsIm5hbWUiOiLsoJXsm5DsmIEiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDQuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1yTmtRTE9EcFl2VS9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BTVp1dWNtejN4OWNuVmpZRXdWQmw2YjhoVFc5WWdTMXJnL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiLsm5DsmIEiLCJmYW1pbHlfbmFtZSI6IuyglSIsImxvY2FsZSI6ImtvIiwiaWF0IjoxNTk5NjE1OTc2LCJleHAiOjE1OTk2MTk1NzZ9.QXgeO2hoeOWFZanqRTUTwZfWnF1s8wJIVT0z-PE8IBSotl23ZPEupG71ezujKXxtpO42zS6KLb01S7RvO4WUuP2CPRk8YtXj9oCQcKuBBOkLu-BXjH09jCmxt_sU_g_7h1dA0BPq62N_4xQz_I_7nhet66S-MjUnPelUVJESuwGzhNFoJCTdczcTbzyUtg9rd_Rxm9DJjbHFwZqQIq9wMUfNmr6mv4Qk2W_AZ7seLd8rnB4aOjuTp1dCSXd3wqPGGsXA25v57HyjP8Gh3_IgxQtZvy4NcOC2PhlUjSUgsGZcQdr-roGTk8q9kyw_McxQWHDiZSEyQU-LKa4lQcm4pw',
-  scopes: [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'openid',
-    'https://www.googleapis.com/auth/userinfo.profile',
-  ],
-  serverAuthCode:
-    '4/3wHAgpfo9zsJYuDRsic72mm5i8xkyJK8vqrZaI9aNFcWVcYL9GwDaHa-Sg_Yj6KxnlbqltHCjkaJhWWFHC0hLqw',
-  user: {
-    email: 'jwy27772415@gmail.com',
-    familyName: '정',
-    givenName: '원영',
-    id: '103109533125112773288',
-    name: '정원영',
-    photo:
-      'https://lh4.googleusercontent.com/-rNkQLODpYvU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmz3x9cnVjYEwVBl6b8hTW9YgS1rg/s120/photo.jpg',
-  },
-};
-
 export default function GoogleLogin({navigation}) {
   const [state, setState] = useState({
     loggedIn: false,
