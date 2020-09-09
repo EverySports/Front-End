334df5a2572925cb9d180e06d5a0d1056235ae95
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2020-09-09 20:27:29 +0900

    Update google_utils.js

--- src/screens/Login/GoogleLogin/google_utils.js
+++ src/screens/Login/GoogleLogin/google_utils.js
@@ -2,11 +2,7 @@ import {GoogleSignin} from '@react-native-community/google-signin';
 
 export function init() {
   GoogleSignin.configure({
-    webClientId:
-      '393763741776-oltarqhe70aodifimjplr27ebgg4o6a2.apps.googleusercontent.com',
-    offlineAccess: true,
-    hostedDomain: '',
-    forceConsentPrompt: true,
+    //비밀이지렁
   });
 }
 
